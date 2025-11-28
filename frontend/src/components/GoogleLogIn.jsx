21ad62b9adcc657f6e21b7726c86f8dc73727a0b
omarfarukcodes69 <omarfaruk.codes69@gmail.com>
2025-11-28 19:18:55 +0600

    feture added

--- frontend/src/components/GoogleLogIn.jsx
+++ frontend/src/components/GoogleLogIn.jsx
@@ -1,7 +1,6 @@
 "use client";
 import useAuth from "@/context/useAuth";
 import { useRouter } from "next/navigation";
-import React from "react";
 import { FcGoogle } from "react-icons/fc";
 import { toast } from "react-toastify";
 
@@ -9,22 +8,23 @@ export default function GoogleLogIn({ redirectPath = "/" }) {
   const { logInGoogle } = useAuth();
   const router = useRouter();
 
-  const handleGoogleLogIn = () => {
-    logInGoogle()
-      .then((res) => {
-        toast.success("Logged in with Google!");
-        router.push(redirectPath); // redirect back
-      })
-      .catch((err) => {
-        toast.error(err.message);
-      });
+  const handleGoogleLogIn = async () => {
+    try {
+      const res = await logInGoogle();
+      console.log("Google login success:", res.user);
+      toast.success("Logged in with Google!");
+      router.push(redirectPath); // login এর পর আগের route এ redirect
+    } catch (err) {
+      console.error(err);
+      toast.error(err.message);
+    }
   };
 
   return (
     <button
       type="button"
       onClick={handleGoogleLogIn}
-      className="btn btn-secondary w-full p-4 flex items-center justify-center gap-2"
+      className="btn btn-secondary w-full p-4 flex items-center justify-center gap-2 mt-3"
     >
       <FcGoogle className="text-2xl" /> Log In With Google
     </button>
